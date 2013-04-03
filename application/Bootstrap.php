ba6dfc5ae360e02aee204030bdc309320719cb4b
Manuel HERVO <mhervo@digitaleo.com>
2013-04-03 17:39:50 +0200

    Ajout des fichiers empty pour commiter les répertoires encore vides. Ajout des fichiers Bootstrap des modules

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,15 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    public function _initAutoload()
+    {
+        $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+            'basePath'  => dirname(__FILE__),
+            'namespace' => '',
+        ));
+        $resourceLoader->addResourceType('services', 'services/', 'Service')
+            ->addResourceType('model', 'models/', 'Model');
+        return $resourceLoader;
+    }
 }
 
---
+++ application/modules/api/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+class Api_Boostrap extends Zend_Application_Module_Bootstrap
+{
+    /**
+     * Application resource namespace
+     * @var false|string
+     */
+    protected $_appNamespace = 'Api';
+}
